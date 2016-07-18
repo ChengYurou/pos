f32d8c77235fa9aaf01e5858c270f8784b0068a8
chengyurou <18829292695@163.com>
2016-07-18 18:01:45 +0800

    use 7 minites

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,14 +1,13 @@
 'use strict';
 
 function buildCartItems(tags) {
-  let cartItems = [];
   let items = loadAllItems();
+  let cartItems = [];
 
   for (let tag of tags) {
-    let barcodeArray = tag.split("-");
+    let barcodeArray = tag.split('-');
     let barcode = barcodeArray[0];
     let count = parseFloat(barcodeArray[1] || 1);
-
     let cartItem = cartItems.find((cartItem) => {
       return cartItem.item.barcode == barcode
     });
@@ -19,6 +18,7 @@ function buildCartItems(tags) {
       let item = items.find((item) => {
         return item.barcode == barcode;
       });
+
       cartItems.push({item: item, count: count});
     }
   }
