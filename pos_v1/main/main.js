36182e2abd851f40f88bd9ea63c83810f1476401
chengyurou <18829292695@163.com>
2016-07-18 16:22:06 +0800

    tasking1

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,3 +1,54 @@
 'use strict';
 
-//TODO: 请在该文件中实现练习要求并删除此注释
+function buildCartItems(barcodes) {
+  let cartItems = [];
+
+  barcodes.forEach(function (barcode) {
+    let barcodeArray = barcode.split("-");
+
+    if(barcodeArray.length > 1){
+      for(let i=0; i<parseInt(barcodeArray[1]); i++){
+        barcodes.splice(barcodes.indexOf(barcode),0,barcodeArray[0]);
+      }
+      barcodes.splice(barcodes.indexOf(barcode),1);
+    }
+  });
+
+  barcodes.forEach(function (barcode) {
+    let existedItem = findItem(barcode);
+    let existedCartItem = findExistCartItem(existedItem,cartItems);
+
+    if (existedCartItem) {
+      existedCartItem.count++;
+    } else {
+      cartItems.push({item: existedItem, count: 1})
+    }
+  });
+
+  return cartItems;
+}
+
+function findExistCartItem(existedItem,cartItems) {
+  let existedCartItem = false;
+
+  cartItems.forEach(function (cartItem) {
+    if(cartItem.item.barcode === existedItem.barcode){
+      existedCartItem = cartItem;
+    }
+  });
+
+  return existedCartItem;
+}
+
+function findItem(barcode) {
+  let existedItem;
+  const items = loadAllItems();
+
+  items.forEach(function (item) {
+    if(item.barcode === barcode){
+      existedItem = item;
+    }
+  });
+
+  return existedItem;
+}
