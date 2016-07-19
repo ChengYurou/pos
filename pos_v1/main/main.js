61cecd9d09187fb1db787b2bec28e9ecf63cc2b6
chengyurou <18829292695@163.com>
2016-07-19 18:26:35 +0800

    accomplish pos_v1

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,21 +1,50 @@
 "use strict";
 
+function printReceipt(receipt) {
+  let cartItemText = getCartItemText(receipt.receiptItems);
+  let receiptText = getReceiptText(receipt);
+  let text = "***<没钱赚商店>收据***" + cartItemText
+    + "\n----------------------" + receiptText + "\n**********************";
+
+  console.log(text);
+}
+
+function getReceiptText(receipt) {
+
+  return '\n总计：' + receipt.total.toFixed(2)
+    + '(元)\n节省：' + receipt.summarySave.toFixed(2) + '(元)';
+}
+
+function getCartItemText(receiptItems) {
+  let cartItemText = "";
+
+  for (let receiptItem of receiptItems) {
+    let cartItem = receiptItem.cartItem;
+    cartItemText += '\n名称：' + cartItem.item.name
+      + '，数量：' + cartItem.count + cartItem.item.unit
+      + '，单价：' + cartItem.item.price.toFixed(2)
+      + '(元)，小计：' + receiptItem.subtotal.toFixed(2) + '(元)';
+  }
+
+  return cartItemText;
+}
+
 function buildCartItems(tags) {
   let cartItems = [];
   let items = loadAllItems();
 
-  for(let tag of tags) {
+  for (let tag of tags) {
     let barcodeArray = tag.split('-');
     let barcode = barcodeArray[0];
-    let count = parseFloat(barcodeArray[1]||1);
+    let count = parseFloat(barcodeArray[1] || 1);
 
     let cartItem = cartItems.find(cartItem => cartItem.item.barcode === barcode);
 
-    if(cartItem) {
+    if (cartItem) {
       cartItem.count++;
-    }else{
+    } else {
       let item = items.find(item => item.barcode === barcode);
-      cartItems.push({item,count});
+      cartItems.push({item, count});
     }
   }
   return cartItems;
@@ -32,12 +61,12 @@ function buildReceiptItems(promotions, cartItems) {
 
 function discount(cartItem, promotionType) {
   let freeCount = 0;
-  if(promotionType === 'BUY_TWO_GET_ONE_FREE') {
-    freeCount  = parseInt(cartItem.count/3);
+  if (promotionType === 'BUY_TWO_GET_ONE_FREE') {
+    freeCount = parseInt(cartItem.count / 3);
   }
 
-  let subtotal = (cartItem.count-freeCount)* cartItem.item.price;
-  let save = freeCount*cartItem.item.price;
+  let subtotal = (cartItem.count - freeCount) * cartItem.item.price;
+  let save = freeCount * cartItem.item.price;
 
   return {subtotal, save};
 }
@@ -46,17 +75,17 @@ function getPromotionType(barcode, promotions) {
   let promotion = promotions.find(promotion => {
     return promotion.barcodes.includes(barcode);
   });
-  return promotion?promotion.type:'';
+  return promotion ? promotion.type : '';
 }
 
 function buildReceipt(receiptItems) {
   let total = 0;
   let summarySave = 0;
-
-  for(let receiptItem of receiptItems) {
+  for (let receiptItem of receiptItems) {
     total += receiptItem.subtotal;
     summarySave += receiptItem.save;
   }
 
-  return {receiptItems, total, summarySave};
+  return {receiptItems, total, summarySave}
 }
+
