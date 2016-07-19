28d041ac3f41f887ff617fa351f6b21a24befe60
chengyurou <18829292695@163.com>
2016-07-19 10:54:39 +0800

    tasking2 my idea

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -3,25 +3,47 @@
 function buildCartItems(tags) {
   let items = loadAllItems();
   let cartItems = [];
-
   for (let tag of tags) {
     let barcodeArray = tag.split('-');
     let barcode = barcodeArray[0];
     let count = parseFloat(barcodeArray[1] || 1);
+
     let cartItem = cartItems.find((cartItem) => {
-      return cartItem.item.barcode == barcode
+      return cartItem.item.barcode === barcode;
     });
 
     if (cartItem) {
-      cartItem.count++;
+      cartItem.count += count;
     } else {
-      let item = items.find((item) => {
-        return item.barcode == barcode;
+      let item = items.find((item)=> {
+        return item.barcode == barcode
       });
-
       cartItems.push({item: item, count: count});
     }
   }
 
   return cartItems;
+}
+
+function buildReceiptItems(promotions, cartItems) {
+  let receiptItems = [];
+
+  for (let cartItem of cartItems) {
+    let promotionBarcode = promotions[0].barcodes.find((barcode) => {
+      return barcode === cartItem.item.barcode;
+    });
+
+    let moreSubtotal = parseFloat(cartItem.item.price * cartItem.count);
+
+    if (promotionBarcode) {
+      let subtotal = (parseInt(cartItem.count / 3) * 2) * cartItem.item.price +
+        parseInt(cartItem.count % 3) * cartItem.item.price;
+      let save = moreSubtotal - subtotal;
+      receiptItems.push({cartItem: cartItem, subtotal: subtotal, save: save});
+    } else {
+      receiptItems.push({cartItem: cartItem, subtotal: moreSubtotal, save: 0})
+    }
+  }
+
+  return receiptItems;
 }
